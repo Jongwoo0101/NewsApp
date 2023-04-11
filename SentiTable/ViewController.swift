5f5359de5df0002774675aa2bae1137f15dedad3
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-04-11 09:58:06 +0900

    Initial Commit

---
+++ SentiTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SentiTable
+//
+//  Created by 원종우 on 2023/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
